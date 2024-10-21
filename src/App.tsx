e014d8550bf3cfe8e6ddac8e83bb61c7978b7138
Pitt Wu <kgb00128@gmail.com>
2024-10-21 22:27:03 +0800

    [Chore] setting prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 const App = () => {
   return (
     <main>
-      <h1 className="text-3xl font-bold underline text-blue-600">
+      <h1 className="text-3xl font-bold text-blue-600 underline">
         Hello world!
       </h1>
+      <button className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
+        ...
+      </button>
     </main>
   );
 };
